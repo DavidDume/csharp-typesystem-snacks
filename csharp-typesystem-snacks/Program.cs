74cef52f39425a184bd112bc14d0aa5f0fe96e03
David Dume <dumedavid11@gmail.com>
2023-09-06 14:14:52 +0200

    Add project files.

---
+++ csharp-typesystem-snacks/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//SNACK 1  
+Console.WriteLine("Inserisci primo numero:");
+int numero1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Inserisci secondo numero:");
+int numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Il numero maggiore è: {(numero1 < numero2 ? numero2 : numero1)}");
